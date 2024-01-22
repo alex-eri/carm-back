e65a8f3c783c5573d12a8b867df3bc1af289ecb4
Aleksandr Stepanov <sa@eri.su>
2024-01-22 22:07:35 +0300

    пример

--- pages/index.vue
+++ pages/index.vue
@@ -4,4 +4,49 @@
     кнопка перехода к справочнику пользователей
  -->
 
-<template></template>
+<template>
+    <div class="container">
+        <b-navbar>
+            <b-navbar-nav>
+                <b-nav-item to="/" title="Расписание">🏠</b-nav-item>
+                <b-nav-item to="/users" title="Пользователи">👤</b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
+
+        <b-table>
+            <caption>
+                Расписание c {{ start }} по {{ stop }}
+            </caption>
+            <b-tbody>
+                <b-tr v-for="s in shedules">
+                    <b-td>
+                        {{ s.start }}-{{ s.stop }}
+                    </b-td>
+                    <b-td>
+                        {{ s }}
+                    </b-td>
+                </b-tr>
+            </b-tbody>
+        </b-table>
+    </div>
+</template>
+
+<script>
+
+export default {
+    async setup() {
+
+        var start = new Date();
+        start.setUTCHours(0, 0, 0, 0);
+
+        var stop = new Date();
+        stop.setUTCHours(23, 59, 59, 999);
+
+        const shedules = await useFetch('/api/shedule/', { query: { start: start.toISOString(), stop: stop.toISOString() } })
+
+        return { shedules, start, stop }
+
+    }
+}
+
+</script>
